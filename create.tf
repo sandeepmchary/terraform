a766c4b8d1060abadf05f6663f6eed5799beaa80
sandeepmchary <awssandeepchary@gmail.com>
2019-11-24 11:52:19 +0530

    added create file for terraform

--- create.tf
+++ create.tf
@@ -1,3 +1,10 @@
 provider "aws"{
 	region = "us-west-2"
 }
+resource "aws_instance" "web_server" {
+  ami = "ami-0c64dd618a49aeee8"
+  instance_type = "t2.micro"
+  tags{
+	  Name = "my_server"
+  }
+}
