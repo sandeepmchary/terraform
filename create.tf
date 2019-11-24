0f7d562920d7fa801a6897caa5245d75e2ebe454
sandeepmchary <awssandeepchary@gmail.com>
2019-11-24 19:14:47 +0530

    added new ami id

--- create.tf
+++ create.tf
@@ -2,7 +2,7 @@ provider "aws"{
 	region = "us-west-2"
 }
 resource "aws_instance" "web_server" {
-  ami = "ami-0c64dd618a49aeee8"
+  ami = "ami-0f2b4fc905b0bd1f1"
   instance_type = "t2.micro"
   
 }
